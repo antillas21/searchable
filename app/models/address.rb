f1c7b885a88e22be1dc228b80b695a1c5ae42ace
Antonio Antillon <antillas21@gmail.com>
2013-02-07 18:05:32 -0800

    Adds relationships between User and Address.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ActiveRecord::Base
-  attr_accessible :city, :country, :section, :state, :street_name, :zip
+  attr_accessible :city, :country, :section, :state, :street_name, :zip, :user
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :nickname, :phone_number
+
+  has_many :addresses
 end
